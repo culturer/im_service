4ed01ebd71b2cf8b0005daf160797914fdda3334
jokor <78901214@qq.com>
2019-10-18 11:51:15 +0800

    修改添加好友逻辑

--- controllers/user.go
+++ controllers/user.go
@@ -1,13 +1,13 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
 	"im_service/models"
 	"time"
 	//"github.com/astaxie/beego/logs"
-
 )
 
 type UserController struct {
@@ -16,32 +16,46 @@ type UserController struct {
 
 //登陆
 const userLogin = 0
+
 //注册
 const userRegister = 1
+
 //搜索
-const userSearch  = 2
+const userSearch = 2
+
 //修改用户信息
-const userUpdate  = 3
+const userUpdate = 3
+
 //充值VIP
-const addUserVip  = 4
+const addUserVip = 4
+
 //新增好友分类
-const addCategory  = 5
+const addCategory = 5
+
 //修改好友分类
-const updateCategory  = 6
+const updateCategory = 6
+
 //删除好友分类
-const delCategory  = 7
+const delCategory = 7
+
 //更新好友列表
-const updateUserList  = 8
+const updateUserList = 8
+
 //删除好友
-const delUserList  = 9
+const delUserList = 9
+
 //添加好友
-const addUserList  = 10
+const addUserList = 10
+
 //获取好友列表
-const getUserList  = 11
+const getUserList = 11
+
 //申请添加好友
-const addApply  = 12
+const addApply = 12
+
 //获取申请列表
-const getApplyList  = 13
+const getApplyList = 13
+
 //查看好友信息
 const getUserInfo = 14
 
@@ -53,45 +67,45 @@ func (this *UserController) Get() {
 
 func (this *UserController) Post() {
 
-	options,err := this.GetInt("options",-1)
+	options, err := this.GetInt("options", -1)
 	this.dealError(err)
 	//检查请求的方法
 	if options != -1 {
 		switch options {
-			case userLogin:
-				this.login()
-			case userRegister:
-				this.register()
-			case userSearch:
-				this.search()
-			case userUpdate:
-				this.update()
-			case addUserVip:
-				this.addVip()
-			case addCategory:
-				this.addCategory()
-			case updateCategory:
-				this.updateCategory()
-			case delCategory:
-				this.delCategory()
-			case updateUserList:
-				this.updateUserList()
-			case delUserList:
-				this.delUserList()
-			case addUserList:
-				this.addUserList()
-			case getUserList:
-				this.getUserList()
-			case addApply:
-				this.addApply()
-			case getApplyList:
-				this.getApplyList()
-			case getUserInfo:
-				this.getUserInfo()
+		case userLogin:
+			this.login()
+		case userRegister:
+			this.register()
+		case userSearch:
+			this.search()
+		case userUpdate:
+			this.update()
+		case addUserVip:
+			this.addVip()
+		case addCategory:
+			this.addCategory()
+		case updateCategory:
+			this.updateCategory()
+		case delCategory:
+			this.delCategory()
+		case updateUserList:
+			this.updateUserList()
+		case delUserList:
+			this.delUserList()
+		case addUserList:
+			this.addUserList()
+		case getUserList:
+			this.getUserList()
+		case addApply:
+			this.addApply()
+		case getApplyList:
+			this.getApplyList()
+		case getUserInfo:
+			this.getUserInfo()
 		default:
-				this.Data["json"] = map[string]interface{}{"status": 400, "msg": "没有对应处理方法", "time": time.Now().Format("2006-01-02 15:04:05")}
-				this.ServeJSON()
-				return
+			this.Data["json"] = map[string]interface{}{"status": 400, "msg": "没有对应处理方法", "time": time.Now().Format("2006-01-02 15:04:05")}
+			this.ServeJSON()
+			return
 		}
 	}
 	this.Data["json"] = map[string]interface{}{"status": 400, "msg": "options is null !", "time": time.Now().Format("2006-01-02 15:04:05")}
@@ -102,13 +116,13 @@ func (this *UserController) Post() {
 //登陆
 func (this *UserController) login() {
 
-	tel := this.GetString("tel","")
-	pwd := this.GetString("pwd","")
-	logs.Info("tel --- ",tel , " || pwd --- "+pwd)
+	tel := this.GetString("tel", "")
+	pwd := this.GetString("pwd", "")
+	logs.Info("tel --- ", tel, " || pwd --- "+pwd)
 
 	var user models.TUser
-	o:= orm.NewOrm()
-	err := o.QueryTable("t_user").Filter("tel",tel).Filter("pwd",pwd).RelatedSel().One(&user)
+	o := orm.NewOrm()
+	err := o.QueryTable("t_user").Filter("tel", tel).Filter("pwd", pwd).RelatedSel().One(&user)
 	this.dealError(err)
 	this.Data["json"] = map[string]interface{}{"status": 200, "user": user, "time": time.Now().Format("2006-01-02 15:04:05")}
 	this.ServeJSON()
@@ -117,18 +131,18 @@ func (this *UserController) login() {
 
 //注册
 func (this *UserController) register() {
-	msg := this.GetString("msg","")		//短信验证码
+	msg := this.GetString("msg", "") //短信验证码
 	o := orm.NewOrm()
 	var user models.TUser
-	user.Tel =  this.GetString("tel","")
-	user.Pwd = this.GetString("pwd","")
+	user.Tel = this.GetString("tel", "")
+	user.Pwd = this.GetString("pwd", "")
 	var family models.TFamily
 	user.Family = &family
-	user.CreateTime =time.Now()
+	user.CreateTime = time.Now()
 	user.LoginTime = time.Now()
-	user.BanTime = time.Now().AddDate(10000,0,0)
-	id,err := o.Insert(&user)
-	logs.Info("register","验证码[",msg,"]",user)
+	user.BanTime = time.Now().AddDate(10000, 0, 0)
+	id, err := o.Insert(&user)
+	logs.Info("register", "验证码[", msg, "]", user)
 	this.dealError(err)
 	this.Data["json"] = map[string]interface{}{"status": 200, "id": id, "time": time.Now().Format("2006-01-02 15:04:05")}
 	this.ServeJSON()
@@ -146,7 +160,7 @@ func (this *UserController) update() {
 }
 
 //充值VIP
-func (this *UserController) addVip(){
+func (this *UserController) addVip() {
 
 }
 
@@ -154,7 +168,6 @@ func (this *UserController) addVip(){
 func (this *UserController) addCategory() {
 
 }
-
 
 //修改好友分类
 func (this *UserController) updateCategory() {
@@ -177,12 +190,15 @@ func (this *UserController) delUserList() {
 }
 
 //获取添加好友列表
-func (this *UserController) getApplyList(){
-	var applies []*models.TApply
-	userId,_ := this.GetInt64("userId",-1)
+func (this *UserController) getApplyList() {
+	var applies []models.TApply
+	userId, _ := this.GetInt64("userId", -1)
 	o := orm.NewOrm()
-	_,err := o.QueryTable("t_apply").Filter("user_id",userId).Distinct().OrderBy("create_time").All(&applies)
-	this.dealError(err)
+	//_,err := o.QueryTable("t_apply").Filter("user_id",userId).Filter("to_id",userId).Distinct().OrderBy("create_time").All(&applies)
+	//this.dealError(err)
+	sql := fmt.Sprintf("select * from t_apply where user_id=%d or to_id=%d order by create_time desc ", userId, userId)
+	logs.Info("sql :", sql)
+	o.Raw(sql).QueryRows(&applies)
 	this.Data["json"] = map[string]interface{}{"status": 200, "applies": applies, "time": time.Now().Format("2006-01-02 15:04:05")}
 	this.ServeJSON()
 	return
@@ -190,26 +206,69 @@ func (this *UserController) getApplyList(){
 
 //申请添加好友
 func (this *UserController) addApply() {
-
+	logs.Info("addApply")
 	o := orm.NewOrm()
 	var apply models.TApply
-	userId,_ := this.GetInt64("userId",-1)
-	friendId,_ := this.GetInt64("friendId",-1)
-	fromCategoryId,_ := this.GetInt64("fromCategoryId",-1)
-	myType,_ := this.GetInt("type",0)
-	msg := this.GetString("msg","")
-	apply.UserId = userId
-	apply.ToId = friendId
+	userId, err := this.GetInt64("userId", -1)
+	this.dealError(err)
+	logs.Info("userId : ", userId)
+	friendId, err := this.GetInt64("friendId", -1)
+	logs.Info("friendId : ", friendId)
+	this.dealError(err)
+	//查询，去重
+	sql := fmt.Sprintf("select * from t_apply where user_id=%d and to_id= friendId ")
+	o.Raw(sql).QueryRow(&apply)
+	//err = o.QueryTable("t_apply").Filter("user_id",userId).Filter("to_id",friendId).One(&apply)
+	this.dealError(err)
+
+	fromCategoryId, err := this.GetInt64("fromCategoryId", -1)
+	this.dealError(err)
+	logs.Info("fromCategoryId : ", fromCategoryId)
+	msg := this.GetString("msg", "")
+	logs.Info("msg : ", msg)
+	fromUsername := this.GetString("fromUsername", "")
+	logs.Info("fromUsername : ", fromUsername)
+	fromIcon := this.GetString("fromIcon", "")
+	logs.Info("fromIcon : ", fromIcon)
+
+	toUsername := this.GetString("toUsername", "")
+	logs.Info("toUsername : ", toUsername)
+	toIcon := this.GetString("toIcon", "")
+	logs.Info("toIcon : ", toIcon)
+
 	apply.Msg = msg
 	apply.CreateTime = time.Now()
-	apply.Type = myType
-	apply.Status = false
 	apply.FromCategoryId = fromCategoryId
-	id,err := o.Insert(apply)
-	this.dealError(err)
-	this.Data["json"] = map[string]interface{}{"status": 200, "id": id, "time": time.Now().Format("2006-01-02 15:04:05")}
-	this.ServeJSON()
-	return
+	apply.FromUsername = fromUsername
+	apply.FromIcon = fromIcon
+	apply.ToUsername = toUsername
+	apply.ToIcon = toIcon
+
+	if &apply != nil && apply.Id != 0 {
+		_, err = o.Update(&apply)
+		logs.Info("update apply", apply)
+		this.dealError(err)
+		this.Data["json"] = map[string]interface{}{"status": 200, "apply": apply, "time": time.Now().Format("2006-01-02 15:04:05")}
+		this.ServeJSON()
+		return
+	} else {
+		myType, err := this.GetInt("type", 0)
+		this.dealError(err)
+		logs.Info("myType : ", myType)
+
+		apply.UserId = userId
+		apply.ToId = friendId
+		apply.Type = myType
+		apply.Status = 0
+
+		_, err = o.Insert(&apply)
+		this.dealError(err)
+
+		this.Data["json"] = map[string]interface{}{"status": 200, "apply": apply, "time": time.Now().Format("2006-01-02 15:04:05")}
+		this.ServeJSON()
+		return
+
+	}
 }
 
 //添加好友
@@ -217,26 +276,26 @@ func (this *UserController) addUserList() {
 	var apply models.TApply
 	var user models.TUser
 	var userList models.TUserList
-	tel := this.GetString("tel","")
-	msg := this.GetString("msg","")
-	userId,err := this.GetInt64("userId",-1)
-	applyId,err := this.GetInt64("applyId",-1)
-	categoryId,err := this.GetInt64("categoryId",-1)
+	tel := this.GetString("tel", "")
+	msg := this.GetString("msg", "")
+	userId, err := this.GetInt64("userId", -1)
+	applyId, err := this.GetInt64("applyId", -1)
+	categoryId, err := this.GetInt64("categoryId", -1)
 	this.dealError(err)
 	o := orm.NewOrm()
-	err = o.QueryTable("t_apply").Filter("id",applyId).One(&apply)
+	err = o.QueryTable("t_apply").Filter("id", applyId).One(&apply)
 	this.dealError(err)
-	apply.Status = true
-	_,err = o.Update(apply)
+	apply.Status = 0
+	_, err = o.Update(apply)
 	this.dealError(err)
-	err = o.QueryTable("t_user").Filter("tel",tel).RelatedSel().One(&user)
+	err = o.QueryTable("t_user").Filter("tel", tel).RelatedSel().One(&user)
 	this.dealError(err)
 	userList.Belong = userId
 	userList.Friend = &user
 	userList.CategoryId = categoryId
 	userList.Msg = msg
 	userList.Sort = 0
-	id,err := o.Insert(userList)
+	id, err := o.Insert(userList)
 	this.Data["json"] = map[string]interface{}{"status": 200, "id": id, "time": time.Now().Format("2006-01-02 15:04:05")}
 	this.ServeJSON()
 	return
@@ -244,28 +303,28 @@ func (this *UserController) addUserList() {
 
 //获取好友列表
 func (this *UserController) getUserList() {
-	userId,err := this.GetInt64("userId",-1)
+	userId, err := this.GetInt64("userId", -1)
 	this.dealError(err)
 	//查好友分类
 	var categorys []*models.TCategory
 	o := orm.NewOrm()
-	_ , err = o.QueryTable("t_category").Filter("user_id",userId).All(&categorys)
+	_, err = o.QueryTable("t_category").Filter("user_id", userId).All(&categorys)
 	this.dealError(err)
 	//查好友
 	var friends []models.TUserList
 	_, err = o.QueryTable("t_user_list").Filter("belong", userId).RelatedSel("friend").All(&friends)
 	this.dealError(err)
-	this.Data["json"] = map[string]interface{}{"status": 200, "categorys":categorys,"friends":friends, "time": time.Now().Format("2006-01-02 15:04:05")}
+	this.Data["json"] = map[string]interface{}{"status": 200, "categorys": categorys, "friends": friends, "time": time.Now().Format("2006-01-02 15:04:05")}
 	this.ServeJSON()
 	return
 }
 
 func (this *UserController) getUserInfo() {
-	tel := this.GetString("tel","")
+	tel := this.GetString("tel", "")
 	if tel != "" {
 		var user models.TUser
-		o:= orm.NewOrm()
-		err := o.QueryTable("t_user").Filter("tel",tel).One(&user)
+		o := orm.NewOrm()
+		err := o.QueryTable("t_user").Filter("tel", tel).One(&user)
 		this.dealError(err)
 		user.Family = nil
 		user.Pwd = ""
@@ -273,14 +332,14 @@ func (this *UserController) getUserInfo() {
 		this.ServeJSON()
 		return
 	}
-	this.Data["json"] = map[string]interface{}{"status": 400,"msg":"unknow tel !","time": time.Now().Format("2006-01-02 15:04:05")}
+	this.Data["json"] = map[string]interface{}{"status": 400, "msg": "unknow tel !", "time": time.Now().Format("2006-01-02 15:04:05")}
 	this.ServeJSON()
 	return
 }
 
 func (this *UserController) dealError(err error) {
-	if err !=nil {
-		logs.Error("UserController",err.Error())
+	if err != nil {
+		logs.Error("UserController", err.Error())
 		this.Data["json"] = map[string]interface{}{"status": 400, "msg": err.Error(), "time": time.Now().Format("2006-01-02 15:04:05")}
 		this.ServeJSON()
 		return
